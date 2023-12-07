02e7758b7b93c5d54a89d2d95484fe3573037e8a
Mateusz Dybich <mateuszdybich1@gmail.com>
2023-12-07 21:16:14 +0100

    initial

---
+++ Gamestore/GameStore.Application/Entities/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.Entities
+{
+    public class Game
+    {
+        public Guid Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Key { get; private set; }
+
+        public string Description { get; set; }
+
+        public Game()
+        {
+        }
+
+        public Game(Guid id, string name, string key)
+        {
+            Id = id;
+            Name = name;
+            Key = key;
+        }
+
+        public Game(Guid id, string name, string key, string description)
+        {
+            Id = id;
+            Name = name;
+            Key = key;
+            Description = description;
+        }
+    }
+}
---
+++ Gamestore/GameStore.Application/Entities/Platform.cs
@@ -0,0 +1,19 @@
+﻿namespace GameStore.Application.Entities;
+
+public enum PlatformType
+{
+    Mobile,
+
+    Browser,
+
+    Desktop,
+
+    Console,
+}
+
+public class Platform
+{
+    public Guid Id { get; private set; }
+
+    public string Type { get; set; }
+}
