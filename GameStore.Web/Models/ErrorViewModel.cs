00b2819f006745f5a1c5d55e1c5ac6b5c28b4477
Mateusz Dybich <mateuszdybich1@gmail.com>
2024-02-24 21:05:38 +0100

    Delete unnecesary folder

--- GameStore.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace GameStore.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
